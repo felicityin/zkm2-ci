16040c9b19e46a82b3a91aff1d4ef3307b0f7825
Angell Li <angell.l@zkm.io>
2024-12-29 22:27:51 +0800

    temp set rustc

--- test_rust.sh
+++ test_rust.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+which rustc
+ls ~/.zkm-toolchain
+
 cd zkm
 cd prover/examples/sha2-rust/host
 ARGS="711e9609339e92b03ddc0a211827dba421f38f9ed8b9d806e1ffdd8c15ffa03d world!" RUST_LOG=info SEG_OUTPUT=/tmp/rust cargo run --release
