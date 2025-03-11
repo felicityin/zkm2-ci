650357ad2e0d385ecf787a4782c50bd3427f139a
Jingping Yin <yinjingping2022@gmail.com>
2025-03-11 11:12:10 +0800

    opt: enable native cpu

--- scripts/test_e2e_groth16.sh
+++ scripts/test_e2e_groth16.sh
@@ -3,4 +3,4 @@
 . "$HOME/.zkm-toolchain/env"
 
 cd zkm2
-ZKM_DEV=true FRI_QUERIES=1 cargo test -r test_e2e_prove_groth16
+RUSTFLAGS="-C target-cpu=native" ZKM_DEV=true FRI_QUERIES=1 cargo test -r test_e2e_prove_groth16
