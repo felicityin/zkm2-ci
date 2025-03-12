453248274fcd006d39ad840a38889b78a4d38adc
Jingping Yin <yinjingping2022@gmail.com>
2025-03-12 10:36:14 +0800

    update git repo url

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,7 +4,7 @@ if [ -d zkm2 ]; then
     git -C zkm2 checkout
     git -C zkm2 pull --rebase
 else
-    git clone git@github.com:felicityin/zkMIPS/zkm2.git
+    git clone git@github.com:zkMIPS/zkm2.git
 fi
 
 if [ -d zkvm-benchmarks ]; then
